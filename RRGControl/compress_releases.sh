d7a6ba7754ccff45882c9b0800ad51822b6f9ffc
Paul <paul@anastatsia-pc>
2026-02-14 01:12:00 +0300

    Add automatic build to compress_releases.sh

--- RRGControl/compress_releases.sh
+++ RRGControl/compress_releases.sh
@@ -8,10 +8,20 @@ function compress()
 	zip -r "bin/Release/$2.zip" "$1"
 }
 
+function build()
+{
+	#Expects RID as $1
+	dotnet publish -c Release -r "$1" --self-contained -p:PublishReadyToRun=true
+}
+
 if [[ -z "$1" ]]; then
     echo "Please specify release version tag!"
     exit 1
 fi
+
+build "linux-x64"
+build "win-x64"
+build "win-x86"
 
 release_name="RRGControl"
 dir="bin/Release/net6.0/linux-x64/publish"
