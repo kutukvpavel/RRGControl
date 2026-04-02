fa6825a28ee193eb6b9f774d7a261fb16af5aa16
Paul <paul@anastatsia-pc>
2026-04-03 01:23:29 +0300

    Add dark theme for preview plot

--- RRGControl/Views/ScriptPreview.axaml.cs
+++ RRGControl/Views/ScriptPreview.axaml.cs
@@ -4,6 +4,7 @@ using AIcon = MsBox.Avalonia.Enums.Icon;
 using System;
 using System.Text;
 using ScottPlot;
+using Avalonia;
 
 namespace RRGControl.Views
 {
@@ -12,6 +13,14 @@ namespace RRGControl.Views
         public ScriptPreview()
         {
             InitializeComponent();
+            DefaultAxesColor = Plot1.Plot.Axes.Bottom.FrameLineStyle.Color;
+            DefaultDataBkgColor = Plot1.Plot.DataBackground.Color;
+            DefaultFigureBkgColor = Plot1.Plot.FigureBackground.Color;
+            DefaultGridColor = Plot1.Plot.Grid.MajorLineColor;
+            DefaultLegendBackgroundColor = Plot1.Plot.Legend.BackgroundColor;
+            DefaultLegendFontColor = Plot1.Plot.Legend.FontColor;
+            DefaultLegendOutlineColor = Plot1.Plot.Legend.OutlineColor;
+            
             DataContextChanged += (o, e) =>
             {
                 if (ViewModel == null) return;
@@ -22,8 +31,16 @@ namespace RRGControl.Views
                 Plot();
             };
             //Loaded += (o, e) => Plot();
+            if (Application.Current != null) Application.Current.ActualThemeVariantChanged += ActualThemeVariant_Changed;
         }
 
+        protected Color DefaultFigureBkgColor;
+        protected Color DefaultDataBkgColor;
+        protected Color DefaultAxesColor;
+        protected Color DefaultGridColor;
+        protected Color DefaultLegendBackgroundColor;
+        protected Color? DefaultLegendFontColor;
+        protected Color DefaultLegendOutlineColor;
         private ViewModels.ScriptPreviewViewModel? ViewModel => DataContext as ViewModels.ScriptPreviewViewModel;
 
         private async void Plot()
@@ -59,6 +76,37 @@ namespace RRGControl.Views
                     icon: AIcon.Warning);
                 await mb.ShowAsync();
             }
+        }
+
+        private void ActualThemeVariant_Changed(object? sender, EventArgs e)
+        {
+            if (App.IsDarkThemed)
+            {
+                // change figure colors
+                Plot1.Plot.FigureBackground.Color = Color.FromHex("#181818");
+                Plot1.Plot.DataBackground.Color = Color.FromHex("#1f1f1f");
+                // change axis and grid colors
+                Plot1.Plot.Axes.Color(Color.FromHex("#d7d7d7"));
+                Plot1.Plot.Grid.MajorLineColor = Color.FromHex("#404040");
+                // change legend colors
+                Plot1.Plot.Legend.BackgroundColor = Color.FromHex("#404040");
+                Plot1.Plot.Legend.FontColor = Color.FromHex("#d7d7d7");
+                Plot1.Plot.Legend.OutlineColor = Color.FromHex("#d7d7d7");   
+            }
+            else
+            {
+                // change figure colors
+                Plot1.Plot.FigureBackground.Color = DefaultFigureBkgColor;
+                Plot1.Plot.DataBackground.Color = DefaultDataBkgColor;
+                // change axis and grid colors
+                Plot1.Plot.Axes.Color(DefaultAxesColor);
+                Plot1.Plot.Grid.MajorLineColor = DefaultGridColor;
+                // change legend colors
+                Plot1.Plot.Legend.BackgroundColor = DefaultLegendBackgroundColor;
+                Plot1.Plot.Legend.FontColor = DefaultLegendFontColor;
+                Plot1.Plot.Legend.OutlineColor = DefaultLegendOutlineColor;
+            }
+            Plot1.Refresh();
         }
     }
 }
